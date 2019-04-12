c3d3adf23bd8f16752a5219485e44eda4fce4080
Softwarica College Lab <softwarica@softwarica.com>
2019-04-12 08:11:35 +0545

    Added

--- app/src/main/java/com/example/listviewapp/MainActivity.java
+++ app/src/main/java/com/example/listviewapp/MainActivity.java
@@ -2,15 +2,44 @@ package com.example.listviewapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView listView;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ListView lvCountries;
+    private Map<String, String> dictionary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dictionary = new HashMap<>();
+        dictionary.put("Nepal","Kathmandu");
+        dictionary.put("India","New Delhi");
+        dictionary.put("China","Bejing");
+        dictionary.put("Japan","Tokya");
+
+        lvCountries = findViewById(R.id.lvCountries);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, new ArrayList<String>(dictionary.keySet()));
+        lvCountries.setAdapter(adapter);
+
+        lvCountries.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String country = parent.getItemAtPosition(position).toString();
+                String capital = dictionary.get(country);
+                Log.d(TAG, capital);
+            }
+        });
     }
 }
